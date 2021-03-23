5d6934268466c02aad035ffd218a18941835459b
Dreknor <Dreknor@users.noreply.github.com>
2021-03-23 16:58:15 +0100

    Update ExpiredPasswordController.php

--- app/Http/Controllers/Auth/ExpiredPasswordController.php
+++ app/Http/Controllers/Auth/ExpiredPasswordController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\PasswordExpiredRequest;
-use Carbon\Carbon;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class ExpiredPasswordController extends Controller
