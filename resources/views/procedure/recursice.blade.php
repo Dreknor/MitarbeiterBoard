31a5d33cffc46280391436ec36ae018a622f83e7
Dreknor <pauli84@freenet.de>
2021-04-20 16:31:12 +0200

    update procedure view

--- resources/views/procedure/recursice.blade.php
+++
@@ -1,8 +0,0 @@
-<li>{{ $step->name }}</li>
-@if (count($step->childs) > 0)
-    <ul>
-        @foreach($step->childs as $newStep)
-            @include('procedure.recursice', $newStep)
-        @endforeach
-    </ul>
-@endif
