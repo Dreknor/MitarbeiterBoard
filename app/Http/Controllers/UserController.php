cafbc9f999c0ecd820bf9612303f64270ee7040e
Dreknor <pauli84@freenet.de>
2021-05-11 10:54:21 +0200

    Update UserController.php
    Fehlerbehebung

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,13 +7,11 @@ use App\Models\ElternInfoBoardUser;
 use App\Models\Group;
 use App\Models\Positions;
 use App\Models\User;
-use Carbon\Carbon;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\View\View;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
@@ -73,7 +71,7 @@ class UserController extends Controller
 
         if (auth()->user()->can('view procedures')) {
             $positions = $request->input('positions');
-            $user->positions->sync($positions);
+            $user->positions()->sync($positions);
 
         }
 
