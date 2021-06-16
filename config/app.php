0885cac6cf1907b1dc3e1fbe0af7f5181dc00f28
Dreknor <pauli84@freenet.de>
2021-06-16 06:36:10 +0200

    add qr

--- config/app.php
+++ config/app.php
@@ -192,6 +192,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class
 
     ],
 
@@ -244,6 +245,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class
 
     ],
 ];
--- resources/views/inventory/items/show.blade.php
+++ resources/views/inventory/items/show.blade.php
@@ -1,23 +1,70 @@
 @extends('layouts.app')
 @section('content')
+    <a href="{{url('inventory/items')}}" class="btn btn-primary btn-link">zurück</a>
+
     <div class="card">
             <div class="row">
-                <div class="col-sm-12 col-md-6">
-                    <div class="card-header">
-                        <div class="container-fluid">
+                <div class="col-sm-12 col-md-9">
+                    <div class="card-header border-bottom">
                             <h5>
                                 {{$item->name}}
                             </h5>
-                        </div>
                     </div>
                     <div class="card-body">
                             {{$item->descripton}}
                     </div>
                     <div class="card-body">
-                        
+                        <div class="card-columns">
+                            <div class="card bg-info">
+                                <div class="card-header">
+                                    <b>
+                                        Kategorie
+                                    </b>
+                                </div>
+                                <div class="card-body">
+                                    {{optional($item->category)->name}}
+                                </div>
+                            </div>
+                            <div class="card bg-warning">
+                                <div class="card-header">
+                                    <b>
+                                        Raum
+                                    </b>
+                                </div>
+                                <div class="card-body">
+                                    {{optional($item->location)->name}}
+                                </div>
+                            </div>
+                            <div class="card bg-primary">
+                                <div class="card-header">
+                                    <b>
+                                        Lieferant
+                                    </b>
+                                </div>
+                                <div class="card-body">
+                                    <p>
+                                        {{optional($item->lieferant)->name}}
+                                    </p>
+                                    <p>
+                                        <b>
+                                            Datum:
+                                        </b> {{optional($item->date)->format('d.m.Y')}}
+                                    </p>
+                                    <p>
+                                        <b>
+                                            Preis:
+                                        </b> {{number_format($item->price,2)}} €
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="card-body">
+                        {!! \SimpleSoftwareIO\QrCode\Facades\QrCode::size(50)->generate(url('inventory/items/'.$item->uuid)); !!}
                     </div>
                 </div>
-                <div class="col-sm-12 col-md-6">
+                <div class="col-sm-12 col-md-3">
                     @if (count($item->getMedia()) > 0)
                         <div id="carouselControls" class="carousel slide" data-ride="carousel">
                             <div class="carousel-inner">
@@ -40,11 +87,10 @@
 
                             </div>
                         @else
-                            <img src="{{asset('img/items.png')}}" style="width: 60%" class="pull-right">
-
+                            <img src="{{asset('img/items.png')}}">
                         @endif
                     </div>
                 </div>
-
+        </div>
     </div>
 @endsection
