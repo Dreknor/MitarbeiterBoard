97b3ee760e9d8658a9dbba86dec1fae747e3ae62
Dreknor <Dreknor@users.noreply.github.com>
2021-09-14 20:33:06 +0200

    Export Inventory Items

--- resources/views/inventory/items/index.blade.php
+++ resources/views/inventory/items/index.blade.php
@@ -155,11 +155,27 @@
 @endsection
 
 @push('js')
-    <script type="text/javascript" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap4.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.11.1/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.11.1/js/dataTables.bootstrap4.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.0.0/js/dataTables.buttons.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.0.0/js/buttons.html5.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.0.0/js/buttons.print.min.js"></script>
+
+
     <script>
         $(document).ready(function () {
-            $('#itemTable').DataTable();
+            $('#itemTable').DataTable({
+                dom: 'Bfrtip',
+                buttons: [
+                    { extend: 'copy', className: 'btn ' },
+                    { extend: 'excel', className: 'btn' },
+                    { extend: 'pdf', className: 'btn' },
+                    { extend: 'print', className: 'btn' },
+                ]
+            } );
         });
 
         $(':checkbox').change(function() {
@@ -180,9 +196,11 @@
 @endpush
 
 @section('css')
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.1/css/jquery.dataTables.min.css"/>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap4.min.css"/>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.bootstrap4.min.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.0.0/css/buttons.dataTables.min.css"/>
+
 
 @endsection
 
