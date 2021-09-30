57662173a5a246de61fa67ffdab69a8c4a5d9336
Dreknor <Dreknor@users.noreply.github.com>
2021-09-30 10:24:13 +0200

    Fehlerbehebung

--- app/Http/Controllers/Inventory/LocationTypeController.php
+++ app/Http/Controllers/Inventory/LocationTypeController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Inventory;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\createLocationTypeRequest;
-use App\Models\Inventory\Inventory\LocationType;
+use App\Models\Inventory\LocationType;
 use Illuminate\Http\Request;
 
 class LocationTypeController extends Controller
