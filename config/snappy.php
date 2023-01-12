0de810b7e13fad88dc35fe1c5788648133446e62
Dreknor <Dreknor@users.noreply.github.com>
2023-01-12 20:33:19 +0100

    add new wkhtmltopdf version

--- config/snappy.php
+++ config/snappy.php
@@ -35,8 +35,9 @@ return [
 
     'pdf' => [
         'enabled' => true,
-        'binary' => base_path('wkhtmltopdf'),
+        'binary' => 'wkhtmltopdf',
         //'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
+        //'binary' => base_path('vendor/lissonpsantos2/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64-ubuntu'),
         'timeout' => false,
         'options' => [],
         'env' => [],
