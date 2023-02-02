a24b8f6e8a87570bad5bca45e10eb598745cfe75
Dreknor <Dreknor@users.noreply.github.com>
2023-02-02 10:08:09 +0100

    add Feiertag to Config

--- config/config.php
+++ config/config.php
@@ -16,7 +16,7 @@ return [
         'Bildungstag' => 100,
         'Entlastungstag' => 100,
         'Feiertag' => 100,
-        'Vorbereitungszeit' => 2.5
+        'Vorbereitungszeit' => 12.5
 
     ],
 
