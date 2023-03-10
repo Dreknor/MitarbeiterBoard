3d32ac0e460b820a063f97477739e3d9cdec117d
Dreknor <Dreknor@users.noreply.github.com>
2023-03-10 07:59:52 +0100

    wiki

--- resources/views/wiki/all.blade.php
+++ resources/views/wiki/all.blade.php
@@ -2,49 +2,10 @@
 
 @section('content')
     <div class="container-fluid">
-
-            <div class="row">
-                <div class="col-5">
-                    <div class="card">
-                        <div class="card-body ">
-                            <form action="{{url('wiki/search')}}" method="post" class="form-inline">
-                                @csrf
-                                <input type="text" class="form-control w-75" name="search" placeholder="Suche ..." required>
-                                <button type="submit" class="btn btn-xs btn-info p-2">
-                                    <i class="fa fa-search" aria-hidden="true"></i> suchen
-                                </button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-                @can('edit wiki')
-                    <div class="col-4">
-                        <div class="card">
-                            <div class="card-body ">
-                                <form action="{{url('wiki/add')}}" method="post" class="form-inline">
-                                    @csrf
-                                    <input type="text" class="form-control w-75" placeholder="Neue Seite" name="title" required>
-                                    <button type="submit" class="btn btn-xs bg-gradient-directional-amber p-2">
-                                        <i class="fa fa-add" aria-hidden="true"></i> erstellen
-                                    </button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                @endcan
-                <div class="col-3">
-                    <div class="card">
-                        <div class="card-body ">
-                            <a href="{{url('wiki/all')}}" class="btn btn-block bg-gradient-directional-blue-grey">
-                                Alle Seiten
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        @include('wiki.header')
         <div class="row">
                 @foreach($letters as $letter)
-                <div class="col-4">
+                <div class="col-12 col-md-4">
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">{{strtoupper($letter)}}</h5>
