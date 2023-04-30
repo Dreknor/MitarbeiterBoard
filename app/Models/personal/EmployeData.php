74c93fca7a3a38d2400d717541e0ea621a5eb5a4
Dreknor <Dreknor@users.noreply.github.com>
2023-04-30 10:05:47 +0200

    export Absence

--- app/Models/personal/EmployeData.php
+++ app/Models/personal/EmployeData.php
@@ -29,13 +29,12 @@ class EmployeData extends Model
 
     protected $hidden = [];
 
-    protected $dates = ['created_at', 'updated_at', 'deleted_at', 'geburtstag'];
-
     protected $casts = [
         'schwerbehindert'   => "boolean",
         'gebutstag' => 'datetime:Y-m-d',
         'caldav_events' => 'boolean',
         'caldav_working_time' => 'boolean',
+        'geburtstag' => 'date'
     ];
 
 
