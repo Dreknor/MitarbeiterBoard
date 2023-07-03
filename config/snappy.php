c97470852ac5e6cb4d7b82f710ee71c2e9cb991f
Dreknor <Dreknor@users.noreply.github.com>
2023-07-03 07:39:10 +0200

    change wkhtmltopdf version

--- config/snappy.php
+++ config/snappy.php
@@ -36,7 +36,7 @@ return [
     'pdf' => [
         'enabled' => true,
         //'binary' => 'wkhtmltopdf',
-        'binary' => base_path('vendor/hcc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
+        'binary' => base_path('vewkhtmltopdf'),
         'timeout' => false,
         'options' => [],
         'env' => [],
