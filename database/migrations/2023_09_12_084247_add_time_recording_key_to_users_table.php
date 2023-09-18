717a3581bb762a49dcae5c9caf7ef2d323a26315
Dreknor <Dreknor@users.noreply.github.com>
2023-09-18 11:16:59 +0200

    timerecording_pin numeric

--- database/migrations/2023_09_12_084247_add_time_recording_key_to_users_table.php
+++ database/migrations/2023_09_12_084247_add_time_recording_key_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('employes_data', function (Blueprint $table) {
-            $table->addColumn('integer','secret_key')->nullable();
+            $table->addColumn('string','secret_key')->nullable();
             $table->addColumn('string','time_recording_key')->nullable()->unique('unique_key');
         });
     }
