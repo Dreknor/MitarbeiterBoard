43b88637900e698d4bd203e71daa08f71159d5c0
Dreknor <Dreknor@users.noreply.github.com>
2023-10-06 07:18:22 +0200

    Fehlerbehebung

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -402,7 +402,7 @@
                             @else
                                 <li class="dropdown">
                                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                                        <img class="avatar border-white" src="{{auth()->user()->photo()}}" alt="..." >
+                                        <img class="avatar-xs border-white" src="{{auth()->user()->photo()}}" alt="..." style="">
                                         <span class="d-none d-md-inline">{{auth()->user()->name}}</span>
                                     </a>
                                     <ul class="dropdown-menu">
