3808b6daf0da95b3976248103c3f7f0620619c68
Dreknor <Dreknor@users.noreply.github.com>
2024-05-21 11:07:47 +0200

    Abwesenheiten Vertretungsplan bearbeiten

--- app/Observers/VertretungsplanAbsenceObserver.php
+++ app/Observers/VertretungsplanAbsenceObserver.php
@@ -24,6 +24,7 @@ class VertretungsplanAbsenceObserver
                         'reason' => $vertretungsplanAbsence->reason,
                         'key' => settings('api_key_elterninfoboard')
                     ]
+                ]);
 
                 Log::log($response->getBody());
 
