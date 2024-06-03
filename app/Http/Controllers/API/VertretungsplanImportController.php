f59fafa99e802d178efe513601ba073427f25b89
Dreknor <Dreknor@users.noreply.github.com>
2024-06-03 21:15:53 +0200

    Fehlerkorrektur Vertretungsplanansicht

--- app/Http/Controllers/API/VertretungsplanImportController.php
+++ app/Http/Controllers/API/VertretungsplanImportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Support\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -10,7 +11,30 @@ class VertretungsplanImportController extends Controller
 {
     public function import(Request $request)
     {
-        Log::info('VertretungsplanImportController:import');
-        Log::info($request->all());
+
+        $data = $request->all();
+
+        Log::info('VertretungsplanImportController import');
+
+        if (is_array($data)) {
+            foreach ($data as $key => $value) {
+                Log::info($key . ' => ' . $value);
+            }
+        }
+
+        $vp_data = $data['Gesamtexport']['Vertretungsplan']['Vertretungsplan'];
+
+        Log::info('VertretungsplanImportController import vp_data');
+        Log::info($vp_data);
+
+
+
+
+
+
+
+
+
+
     }
 }
