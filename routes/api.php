97f3f8acf2c695a6a9ff658c8a6f1768fb0ed8fe
Dreknor <Dreknor@users.noreply.github.com>
2024-06-03 17:42:01 +0200

    Import Vertretungsplan from Indiware - Test

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\VertretungsplanImportController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::put('/vertretungen/vp', 'VertretungsplanImportController@import');
+Route::put('/vertretungen/vp', [VertretungsplanImportController::class, 'import']);
 
