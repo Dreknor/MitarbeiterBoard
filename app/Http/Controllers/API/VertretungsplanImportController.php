bfb4bc9709c4b3102daafb9534265d2af3d99998
Dreknor <Dreknor@users.noreply.github.com>
2024-06-03 21:40:52 +0200

    Test VP Import

--- app/Http/Controllers/API/VertretungsplanImportController.php
+++ app/Http/Controllers/API/VertretungsplanImportController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Klasse;
+use App\Models\User;
+use App\Models\Vertretung;
 use App\Support\Collection;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -14,19 +18,51 @@ class VertretungsplanImportController extends Controller
 
         $data = $request->all();
 
-        Log::info('VertretungsplanImportController import');
-
-        if (is_array($data)) {
-            foreach ($data as $key => $value) {
-                Log::info($key);
-                Log::info($value);
-            }
-        }
-
         $vp_data = $data['Gesamtexport']['Vertretungsplan']['Vertretungsplan'];
 
-        Log::info('VertretungsplanImportController import vp_data');
-        Log::info($vp_data);
+        //Log::info('VertretungsplanImportController import vp_data');
+        //Log::info($vp_data);
+
+        $date = Carbon::createFromFormat('d.m.Y',$vp_data['Kopf']['Datum']);
+
+        Log::info('Datum: '.$date->format('Y-m-d'));
+
+        $absences_array = $vp_data['Kopf']['Kopfinfo']['AbwesendeLehrer'];
+
+        $absences = User::whereIn('kuerzel',$absences_array['Kurz'])->get();
+
+        Log::info('Abwesende Lehrer: ');
+        Log::info($absences);
+
+        $aktionen = $vp_data['Aktionen'];
+
+        foreach ($aktionen as $aktion){
+            $klassen = Klasse::whereIn('name',$aktion['Klassen'])->get();
+
+            Log::info('Klassen: ');
+            Log::info($klassen);
+
+            foreach ($klassen as $klasse){
+
+
+                $vertretung = new Vertretung([
+                    'date' => $date,
+                    'klassen_id' => $klasse->id,
+                    'stunde' => $aktion['Ak_StundeVon'],
+                    'Doppelstunde' => array_key_exists('Ak_StundenAnz',$aktion) ? true : false,
+                    'altFach' => $aktion['Ak_Fach'],
+                    'neuFach' => $aktion['Ak_VFach'],
+                    'user_id' => array_key_exists('VLehrer', $aktion) ? User::where('kuerzel',$aktion['VLehrer'][0])->first()->id : null,
+                    'comment' => array_key_exists('VRaeume', $aktion) ? 'Raum: '.$aktion['VRaeume'][0] : null,
+                ]);
+
+                Log::info('Vertretung: ');
+                Log::info($vertretung);
+
+            }
+
+
+        }
 
 
 
