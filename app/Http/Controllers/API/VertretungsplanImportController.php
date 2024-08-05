fe688850fd15cebd33ad8d96547144611dc72c77
Dreknor <Dreknor@users.noreply.github.com>
2024-08-05 15:38:47 +0200

    Fehlersuche Wiederkehrende Prozesse

--- app/Http/Controllers/API/VertretungsplanImportController.php
+++ app/Http/Controllers/API/VertretungsplanImportController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Klasse;
 use App\Models\User;
 use App\Models\Vertretung;
+use App\Models\VertretungsplanAbsence;
 use App\Support\Collection;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -13,65 +14,124 @@ use Illuminate\Support\Facades\Log;
 
 class VertretungsplanImportController extends Controller
 {
-    public function import(Request $request)
+    public function import()
     {
 
-        $data = $request->all();
+        $data = json_decode(file_get_contents(storage_path('Vertretungsplan.json')), true);
+        foreach ($data as $day){
+            $day = $day[0];
 
-        $vp_data = $data['Gesamtexport']['Vertretungsplan']['Vertretungsplan'][0];
+            try {
+                $date = Carbon::createFromFormat('d.m.Y', $day['Kopf']['Datum']);
+                Log::info('Parsing date: ' . $date);
+                //Abwesenheiten
+                if (array_key_exists('Kopf', $day) && array_key_exists('Kopfinfo', $day['Kopf']) && array_key_exists('AbwesendeLehrer', $day['Kopf']['Kopfinfo'])) {
+                    try {
+                        foreach ($day['Kopf']['Kopfinfo']['AbwesendeLehrer'] as $abwesender) {
+                            $user = User::where('kuerzel', $abwesender['Kurz'])->first();
+                            if ($user) {
+                                $absence = VertretungsplanAbsence::where('user_id', $user->id)
+                                    ->whereDate('start_date', '<=',$date)
+                                    ->whereDate('end_date', '>=',$date)
+                                    ->first();
+                                if (!$absence) {
+                                    $absence = new VertretungsplanAbsence([
+                                        'user_id' => $user->id,
+                                        'start_date' => $date,
+                                        'end_date' => $date,
+                                    ]);
+                                    $absence->save();
+                                }
+                            } else {
+                                Log::info('Lehrer nicht gefunden: ' . $abwesender['Kurz']);
+                            }
+                        }
+                    } catch (\Exception $e) {
+                        Log::error('Error while parsing Abwesenheiten: ');
+                        Log::error($e->getMessage());
+                    }
+                }
 
-        //Log::info('VertretungsplanImportController import vp_data');
-        //Log::info($vp_data);
+                //Vertretungen
+                if (array_key_exists('Aktionen', $day)){
+                    try {
+                        foreach ($day['Aktionen'] as $aktion){
+                            if (array_key_exists('Ak_DatumVon', $day)){
+                                $date = Carbon::createFromFormat('d.m.Y', $day['Ak_DatumVon']);
+                            }
 
-        $date = Carbon::createFromFormat('d.m.Y',$vp_data['Kopf']['Datum']);
+                            if (array_key_exists('VLehrer', $aktion)){
+                                $lehrer = User::where('kuerzel', $aktion['VLehrer'][0])->first();
 
-        Log::info('Datum: '.$date->format('Y-m-d'));
+                            }
+                            if (array_key_exists('VKlassen', $aktion)){
+                                $klassen = Klasse::whereIn('name', $aktion['VKlassen'])->get();
+                            }
 
-        $absences_array = $vp_data['Kopf']['Kopfinfo']['AbwesendeLehrer'];
+                            $type = '';
 
-        $absences = User::whereIn('kuerzel',$absences_array['Kurz'])->get();
+                            switch ($aktion['Ak_Art']){
+                                case 'Änd.':
+                                    if (array_key_exists('Ak_Fach', $aktion) && array_key_exists('Ak_VFach', $aktion) && $aktion['Ak_Fach'] != $aktion['Ak_VFach']){
+                                        $type = 'Vertretung (fachfremd)';
+                                    } else {
+                                        $type = 'Vertretung (fachgerecht)';
+                                    }
+                                    break;
 
-        Log::info('Abwesende Lehrer: ');
-        Log::info($absences);
+                                default:
+                                    $type = 'Ausfall';
+                                    break;
+                            }
 
-        $aktionen = $vp_data['Aktionen'];
+                            foreach ($klassen as $klasse) {
 
-        foreach ($aktionen as $aktion){
-            $klassen = Klasse::whereIn('name',$aktion['Klassen'])->get();
+                                $vertretung = Vertretung::query()
+                                    ->where('klassen_id', $klasse->id)
+                                    ->where('date', $date->format('Y-m-d'))
+                                    ->where('stunde', $aktion['Ak_StundeVon'])
+                                    ->first();
+                                if ($vertretung) {
+                                    $vertretung->update([
+                                        'users_id' => $lehrer?->id,
+                                        'Doppelstunde' => array_key_exists('Ak_Doppelstunde', $aktion) ? true : false,
+                                        'altFach' => $aktion['Ak_Fach'],
+                                        'neuFach' => (array_key_exists('Ak_VFach', $aktion) && $aktion['Ak_VFach'] != "") ? $aktion['Ak_VFach'] : 'Ausfall',
+                                        'type' => $type,
+                                        'comment' => (array_key_exists('Raeume', $aktion) && array_key_exists('VRaeume', $aktion) && $aktion['Raeume'][0] != $aktion['VRaeume'][0]) ? 'Raum: '.$aktion['VRaeume'][0]  : null,
+                                    ]);
+                                } else {
+                                    $vertretung = new Vertretung([
+                                        'klassen_id' => $klasse->id,
+                                        'date' => $date,
+                                        'stunde' => $aktion['Ak_StundeVon'],
+                                        'users_id' => $lehrer?->id,
+                                        'Doppelstunde' => array_key_exists('Ak_Doppelstunde', $aktion) ? true : false,
+                                        'altFach' => $aktion['Ak_Fach'],
+                                        'neuFach' => (array_key_exists('Ak_VFach', $aktion)) ? $aktion['Ak_VFach'] : 'Ausfall',
+                                        'created_at' => Carbon::now(),
+                                        'akt_id' => $aktion['Ak_Id'],
+                                        'type' => $type,
+                                        'comment' => (array_key_exists('Raeume', $aktion) && array_key_exists('VRaeume', $aktion) && $aktion['Raeume'][0] != $aktion['VRaeume'][0]) ? 'Raum: '.$aktion['VRaeume'][0]  : null,
+                                    ]);
+                                    $vertretung->save();
+                                }
+                            }
+                        }
+                    } catch (\Exception $e) {
+                        Log::error('Error while parsing Aktionen: ');
+                        Log::error($e->getMessage());
+                    }
 
-            Log::info('Klassen: ');
-            Log::info($klassen);
-
-            foreach ($klassen as $klasse){
 
 
-                $vertretung = new Vertretung([
-                    'date' => $date,
-                    'klassen_id' => $klasse->id,
-                    'stunde' => $aktion['Ak_StundeVon'],
-                    'Doppelstunde' => array_key_exists('Ak_StundenAnz',$aktion) ? true : false,
-                    'altFach' => $aktion['Ak_Fach'],
-                    'neuFach' => $aktion['Ak_VFach'],
-                    'user_id' => array_key_exists('VLehrer', $aktion) ? User::where('kuerzel',$aktion['VLehrer'][0])->first()->id : null,
-                    'comment' => array_key_exists('VRaeume', $aktion) ? 'Raum: '.$aktion['VRaeume'][0] : null,
-                ]);
 
-                Log::info('Vertretung: ');
-                Log::info($vertretung);
-
+                }
+            } catch (\Exception $e) {
+                Log::error('Error while parsing: ');
+                Log::error($e->getMessage());
+                continue;
             }
-
-
         }
-
-
-
-
-
-
-
-
-
-
     }
 }
