cf9308d2311b1ff9556b4a760dedfcd2691303b6
Dreknor <Dreknor@users.noreply.github.com>
2024-08-16 15:25:43 +0200

    Indiwareimport InfoKlassen

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -28,9 +28,7 @@ class Group extends Model
 
     public function users()
     {
-        return $this->belongsToMany(User::class)->withDefault([
-            'name' => 'System / gelöschter Benutzer',
-        ]);
+        return $this->belongsToMany(User::class);
     }
 
     public function presences(): \Illuminate\Database\Eloquent\Relations\HasMany
