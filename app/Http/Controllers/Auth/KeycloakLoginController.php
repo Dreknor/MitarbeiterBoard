0a7f2cadaad333d9df4bb905414e19b30a9958a6
Dreknor <Dreknor@users.noreply.github.com>
2024-09-19 12:15:14 +0200

    keycloak

--- app/Http/Controllers/Auth/KeycloakLoginController.php
+++ app/Http/Controllers/Auth/KeycloakLoginController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Group;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
+use Spatie\Permission\Models\Role;
 
 class KeycloakLoginController extends Controller
 {
@@ -30,7 +32,42 @@ class KeycloakLoginController extends Controller
         $laravelUser = User::where('username', $user->nickname)
             ->orWhere('email', $user->email)
             ->first();
-        dd($user->user);
+       // dd($user->user);
+        foreach ($user->memberOf as $memberOf){
+            $cn = explode('-',$memberOf);
+            dd($cn);
+            if (!is_null($cn) and count($cn) > 1){
+                $groups_cn = Group::whereIn('name', config('config.auth.set_groups'))->get();
+                $laravelUser->groups_rel()->attach($groups_cn);
+            }
+
+        }
+        if (!$laravelUser) {
+            $laravelUser = User::create([
+                'username' => $user->nickname,
+                'email' => $user->email,
+                'password' => bcrypt($user->sub),
+            ]);
+
+
+
+
+                if (config('config.auth.set_groups') != "" and is_array(config('config.auth.set_groups') )){
+                    $groups_env = Group::whereIn('name', config('config.auth.set_groups'))->get();
+                    $laravelUser->groups_rel()->attach($groups_env);
+                }
+
+
+            if (config('config.auth.set_roles') != "" and is_array(config('config.auth.set_roles') )){
+                $roles = Role::whereIn('name', config('config.auth.set_roles'))->get();
+                $laravelUser->roles()->attach($roles);
+
+            }
+
+        }
+
+
+
 
         Auth::loginUsingId($laravelUser->id);
 
