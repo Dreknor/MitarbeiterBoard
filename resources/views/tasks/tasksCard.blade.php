d43b52005c5702be46c10810dddc551bd94914b3
Dreknor <Dreknor@users.noreply.github.com>
2024-09-29 08:50:18 +0200

    Fehler SortByDate

--- resources/views/tasks/tasksCard.blade.php
+++ resources/views/tasks/tasksCard.blade.php
@@ -14,7 +14,7 @@
                                 <li class="list-group-item word-wrap mt-1  @if($task?->date->lessThan(Carbon\Carbon::now()->addDays(config('config.tasks.remind')))) bg-gradient-directional-amber  @endif">
                                 <p class="word-wrap">
                                     <b>
-                                        {{$task->date->format('d.m.Y')}} - {{$task->taskable->name}}:
+                                        {{$task?->date->format('d.m.Y')}} - {{$task->taskable->name}}:
                                     </b>
                                      {{$task->task}}
                                         <div class="pull-right ml-1">
