05d6dcfc76b6709a84fc72ad6c7af95fe5d988e5
Dreknor <Dreknor@users.noreply.github.com>
2024-11-20 11:01:26 +0100

    Absence Dashboardcard

--- resources/views/absences/dashboardCard.blade.php
+++ resources/views/absences/dashboardCard.blade.php
@@ -169,7 +169,9 @@
     <script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
     <script>
         $(document).ready( function () {
-            $('#absenceTable').DataTable();
+            $('#absenceTable').DataTable({
+                ordering: false
+            });
         } );
     </script>
 
