26d7da0aa2874f1a818207e7293907d616751d9d
Dreknor <Dreknor@users.noreply.github.com>
2025-02-02 15:09:05 +0100

    Update Holidays

--- resources/views/personal/holidays/partials/holidays_overview.blade.php
+++ resources/views/personal/holidays/partials/holidays_overview.blade.php
@@ -15,10 +15,10 @@
             </thead>
             <tbody>
             @forelse(auth()->user()->holidays->filter(function($holiday) use ($month) {
-                return $holiday->start_date->between($month->copy()->startOfMonth(), $month->copy()->endOfYear());
+                return $holiday->start_date->between($month->copy()->startOfYear(), $month->copy()->endOfYear());
             })->sortBy('start_date') as $holiday)
 
-                <tr class="@foreach(auth()->user()->groups_rel as $group) {{$group->name}} @endforeach">
+                <tr class="@foreach(auth()->user()->groups_rel as $group) {{$group->name}} @endforeach @if($holiday->start_date->isFuture()) table-warning @else bg-light-gray @endif">
                     <td>{{ $holiday->start_date->format('d.m.Y') }}</td>
                     <td>{{ $holiday->end_date->format('d.m.Y') }}</td>
                     <td>{{ $holiday->days }}</td>
@@ -44,9 +44,28 @@
                 </tr>
             @endforelse
             </tbody>
+            <tfoot>
+            <tr>
+                <td></td>
+                <td>
+                    <strong>Summe:</strong>
+                </td>
+                <th>
+                    @php
+                        $sum = auth()->user()->holidays->filter(function($holiday) use ($month) {
+                            return $holiday->start_date->between($month->copy()->startOfYear(), $month->copy()->endOfYear());
+                        })->sum('days');
+                    @endphp
+                    {{ $sum }}
+                </th>
+                <td>
+                    <strong>Rest:</strong> {{ auth()->user()->holiday_claim->last() ? auth()->user()->holiday_claim->last()->holiday_claim - $sum : settings('holiday_claim') - $sum }}
+                </td>
+            </tfoot>
         </table>
     </div>
 </div>
+@can('approve holidays')
 <div class="row">
     <div class="col-md-12">
         <div class="card">
@@ -65,7 +84,6 @@
                     </tr>
                     </thead>
                     <tbody>
-                    @can('approve holidays')
                         @foreach($users as $user)
                             <tr class="@foreach($user->groups_rel as $group) {{$group->name}} @endforeach">
                                 <td class="border-right w-25">
@@ -88,19 +106,7 @@
                                 </td>
                             </tr>
                         @endforeach
-                    @else
-                        <tr class="">
-                            <td class="border-right w-25">
-                                {{auth()->user()->name}}
-                            </td>
-                            <td class="border-right">
-                                {{auth()->user()->holidays_date($month->copy()->startOfYear(), \Carbon\Carbon::now())->sum('days')}}
-                                 / {{auth()->user()->holidays_date(\Carbon\Carbon::now()->addDay(), $month->copy()->endOfYear())->sum('days_requested')}}
-                            </td>
-                            <td class="border-right">
 
-                            </td>
-                    @endcan
                     </tbody>
                 </table>
             </div>
@@ -108,3 +114,4 @@
 
     </div>
 </div>
+@endcan
