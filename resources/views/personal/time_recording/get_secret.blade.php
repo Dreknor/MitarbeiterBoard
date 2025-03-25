ed9e4dce86452a465883d18260f828462206c5e3
Dreknor <pauli84@freenet.de>
2025-03-25 12:20:41 +0100

    Refactor get_secret.blade.php layout and improve button structure

--- resources/views/personal/time_recording/get_secret.blade.php
+++ resources/views/personal/time_recording/get_secret.blade.php
@@ -20,49 +20,52 @@
                         <p id="pin_show"  class="mx-auto text-center text-light">
                             Pin:
                         </p>
-                        <div class="container-fluid">
+                        <div class="w-100 mx-auto">
                             <div class="row">
-                                <div class="col-2">
-                                    <button class="btn btn-info" onclick="addNumber(1)">1</button>
+                                <div class="col-auto mx-auto">
+                                    <button class="btn btn-lg btn-info" onclick="addNumber(1)">1</button>
                                 </div>
-                                <div class="col-2">
-                                    <button class="btn btn-sm btn-info" onclick="addNumber(2)">2</button>
+                                <div class="col-auto mx-auto">
+                                    <button class="btn btn-lg btn-info" onclick="addNumber(2)">2</button>
                                 </div>
-                                <div class="col-sm-2 col-md-2 col-lg-1">
-                                    <button class="btn btn-sm btn-info" onclick="addNumber(3)">3</button>
-                                </div>
-                                <div class="col-sm-2 col-md-2 col-lg-1">
-                                    <button class="btn btn-sm btn-info" onclick="addNumber(4)">4</button>
-                                </div>
-                                <div class="col-sm-2 col-md-2 col-lg-1">
-                                    <button class="btn btn-sm btn-info" onclick="addNumber(5)">5</button>
-                                </div>
-                                <div class="col-sm-2 col-md-2 col-lg-1 ">
-                                    <button class="btn btn-sm btn-info" onclick="addNumber(6)">6</button>
-                                </div>
-                                <div class="col-sm-2 col-md-2 col-lg-1 ">
-                                    <button class="btn btn-sm btn-info" onclick="addNumber(7)">7</button>
-                                </div>
-                                <div class="col-sm-2 col-md-2 col-lg-1">
-                                    <button class="btn btn-sm btn-info" onclick="addNumber(8)">8</button>
-                                </div>
-                                <div class="col-sm-2 col-md-2 col-lg-1">
-                                    <button class="btn btn-sm btn-info" onclick="addNumber(9)">9</button>
-                                </div>
-                                <div class="col-sm-2 col-md-2 col-lg-1">
-                                    <button class="btn btn-sm btn-info" onclick="addNumber(0)">0</button>
+                                <div class="col-auto mx-auto">
+                                    <button class="btn btn-lg btn-info" onclick="addNumber(3)">3</button>
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="col-auto">
+                                <div class="col-4 mx-auto">
+                                    <button class="btn btn-lg btn-info" onclick="addNumber(4)">4</button>
+                                </div>
+                                <div class="col-4 mx-auto">
+                                    <button class="btn btn-lg btn-info" onclick="addNumber(5)">5</button>
+                                </div>
+                                <div class="col-4 mx-auto">
+                                    <button class="btn btn-lg btn-info" onclick="addNumber(6)">6</button>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-4 mx-auto">
+                                    <button class="btn btn-lg btn-info" onclick="addNumber(7)">7</button>
+                                </div>
+                                <div class="col-4 mx-auto">
+                                    <button class="btn btn-lg btn-info" onclick="addNumber(8)">8</button>
+                                </div>
+                                <div class="col-4 mx-auto">
+                                    <button class="btn btn-lg btn-info" onclick="addNumber(9)">9</button>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-4 mx-auto">
                                     <button class="btn btn-block btn-success" onclick="submitForm()">absenden</button>
                                 </div>
-                                <div class="col-auto ">
+                                <div class="col-4 mx-auto">
+                                    <button class="btn btn-lg btn-info" onclick="addNumber(0)">0</button>
+                                </div>
+                                <div class="col-4 mx-auto">
                                     <button class="btn btn-block btn-danger" onclick="clearInput()">Eingabe löschen</button>
                                 </div>
                             </div>
                         </div>
-                    </div>
                         <div class="card-body">
                             <div class="w-25 mx-auto">
                                 <div class="text-center text-light">
