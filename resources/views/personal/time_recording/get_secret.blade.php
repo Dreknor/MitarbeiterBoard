bb418a6098cd4aef6c0785f1b685753200cf2967
Dreknor <pauli84@freenet.de>
2025-03-25 12:17:01 +0100

    Refactor get_secret.blade.php layout and improve button structure

--- resources/views/personal/time_recording/get_secret.blade.php
+++ resources/views/personal/time_recording/get_secret.blade.php
@@ -22,34 +22,34 @@
                         </p>
                         <div class="container-fluid">
                             <div class="row">
-                                <div class="col-auto">
+                                <div class="col-sm-2 col-md-2 col-lg-1">
                                     <button class="btn btn-sm btn-info" onclick="addNumber(1)">1</button>
                                 </div>
-                                <div class="col-auto">
+                                <div class="col-sm-2 col-md-2 col-lg-1">
                                     <button class="btn btn-sm btn-info" onclick="addNumber(2)">2</button>
                                 </div>
-                                <div class="col">
+                                <div class="col-sm-2 col-md-2 col-lg-1">
                                     <button class="btn btn-sm btn-info" onclick="addNumber(3)">3</button>
                                 </div>
-                                <div class="col">
+                                <div class="col-sm-2 col-md-2 col-lg-1">
                                     <button class="btn btn-sm btn-info" onclick="addNumber(4)">4</button>
                                 </div>
-                                <div class="col">
+                                <div class="col-sm-2 col-md-2 col-lg-1">
                                     <button class="btn btn-sm btn-info" onclick="addNumber(5)">5</button>
                                 </div>
-                                <div class="col ">
+                                <div class="col-sm-2 col-md-2 col-lg-1 ">
                                     <button class="btn btn-sm btn-info" onclick="addNumber(6)">6</button>
                                 </div>
-                                <div class="col ">
+                                <div class="col-sm-2 col-md-2 col-lg-1 ">
                                     <button class="btn btn-sm btn-info" onclick="addNumber(7)">7</button>
                                 </div>
-                                <div class="col">
+                                <div class="col-sm-2 col-md-2 col-lg-1">
                                     <button class="btn btn-sm btn-info" onclick="addNumber(8)">8</button>
                                 </div>
-                                <div class="col">
+                                <div class="col-sm-2 col-md-2 col-lg-1">
                                     <button class="btn btn-sm btn-info" onclick="addNumber(9)">9</button>
                                 </div>
-                                <div class="col">
+                                <div class="col-sm-2 col-md-2 col-lg-1">
                                     <button class="btn btn-sm btn-info" onclick="addNumber(0)">0</button>
                                 </div>
                             </div>
