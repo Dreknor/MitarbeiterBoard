6432e93d85fbf19606db25b2d00753c541fbc909
Dreknor <Dreknor@users.noreply.github.com>
2025-04-30 12:33:27 +0200

    Fehlersuche

--- app/helpers.php
+++ app/helpers.php
@@ -136,7 +136,6 @@ function is_holiday(Carbon $date)
                 $object->date = $date->format('Y-m-d');
                 $object->title = $item['title'];
 
-                Log::info($object);
                 return $object;
             }
 
