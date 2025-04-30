68d41011a16831849306dee672431f42b697535a
Dreknor <Dreknor@users.noreply.github.com>
2025-04-30 12:26:07 +0200

    Fehlersuche

--- app/helpers.php
+++ app/helpers.php
@@ -131,7 +131,7 @@ function is_holiday(Carbon $date)
         // Datum auf Feiertag prüfen
         return $holidays->first(function ($item) use ($date) {
             if ($item['date'] == $date->format('Y-m-d')){
-                return $item;
+                return (object) $item;
             }
         });
 
