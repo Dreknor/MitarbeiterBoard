041cbf94b3c08724a65e2a52c83d20e24e58743d
Dreknor <Dreknor@users.noreply.github.com>
2025-05-16 07:43:28 +0200

    Fix media retrieval for ticket files in show view

--- resources/views/ticketsystem/show.blade.php
+++ resources/views/ticketsystem/show.blade.php
@@ -56,7 +56,7 @@
                 <ul class="list-group">
                     @foreach($show_ticket->getMedia('ticket_files') as $file)
                         <li class="list-group-item">
-                            <a href="{{ route('tickets.download', $file->id) }}">{{ $file->name }}</a>
+                            <a href="{{url('/image/'.$file->id)}}">{{ $file->name }}</a>
                         </li>
                     @endforeach
                 </ul>
