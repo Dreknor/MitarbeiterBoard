cf8861ff808d2cc858832929c13b13ac21636d78
Dreknor <Dreknor@users.noreply.github.com>
2025-09-11 07:56:43 +0200

    Enhance week display: add dynamic appointment rendering and color coding for classes

--- resources/views/paedDiary/week/day.blade.php
+++ resources/views/paedDiary/week/day.blade.php
@@ -1,11 +1,11 @@
 <ul class="list-group">
 @foreach($appointments as $appointment)
-   <li class=" d-flex justify-content-between align-items-center">
+   <li class="d-flex justify-content-between align-items-center">
        <div class="row">
-           <div class="col-6">
+           <div class="col text-start">
                 {{$appointment['title']}}
            </div>
-           <div class="col-6">
+           <div class="col text-end">
                 {{$appointment['start_time']->format('H:i')}} - {{$appointment['end_time']->format('H:i')}}
            </div>
        </div>
