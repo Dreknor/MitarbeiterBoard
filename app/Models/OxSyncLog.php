9c2c7c448cff0b07bb05fe62ae8935afe92159b0
Dreknor <pauli84@freenet.de>
2026-03-13 13:17:44 +0100

    Add Open-Xchange CalDAV integration and related database migrations

--- app/Models/OxSyncLog.php
+++ app/Models/OxSyncLog.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class OxSyncLog extends Model
 {
+    use HasFactory;
     protected $table = 'ox_sync_log';
 
     protected $fillable = [
@@ -33,4 +35,5 @@ class OxSyncLog extends Model
         return $this->belongsTo(User::class, 'user_id');
     }
 }
+
 
